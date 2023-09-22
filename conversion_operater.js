41ddad58728e895d71fbd660f1907f4a0dad347b
Tayyab94 <tayyab.bhatti30@gmail.com>
2023-09-22 21:55:39 +0500

    conversion_operater

---
+++ conversion_operater.js
@@ -0,0 +1,7 @@
+
+
+let stringNumber = 123;
+console.log(typeof stringNumber);
+let number = String(stringNumber);
+console.log(number);
+console.log(typeof number);
--- datatypes.js
+++ datatypes.js
@@ -1,7 +1,8 @@
 let user_name = "Tayyab";
 
-console.log(user_name);
-console.log(typeof user_name);
+// console.log(user_name);
+// console.log(typeof user_name);
 
-console.log(typeof null); // object
-console.log(typeof undefined) // undefined
+// console.log(typeof null); // object
+// console.log(typeof undefined) // undefined
+
